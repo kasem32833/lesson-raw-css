b4a06224a86842ad0cc97096afefe4ccc23e3e9e
Md Abul Kasem <abulkasem32833@gmail.com>
2025-06-06 00:18:11 +0300

    testimonial slider issue fixed by adding and extra div

--- js/main.js
+++ js/main.js
@@ -11,6 +11,18 @@ YTplayerOverlay.addEventListener('click', ()=>{
 })
 
 
+// testimonial slider
+
+$('.testimonial-wrapper').slick({
+    arrows:false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: true,
+
+})
+
 // blog slider
 $('.blog-wrapper').slick({
     slidesToShow: 3,
